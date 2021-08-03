984c690af4ab0b4b9fc682562e5a12f3d488b92c
DaveCivitello <44002064+DaveCivitello@users.noreply.github.com>
2021-08-03 08:42:35 -0400

    Add files via upload

--- DEB_IBM_for_Experiment.R
+++ DEB_IBM_for_Experiment.R
@@ -2,10 +2,7 @@
 # Loading packages
 library(Matrix)
 library(deSolve)
-library("mvtnorm")
-library(LaplacesDemon)
-library(coda)
-library(adaptMCMC)
+library(dplyr)
 
 # Rtools is needed for compiling the .c file of the DEB model
 rtools <- "C:\\Rtools\\bin"
@@ -43,6 +40,10 @@ pars["DV"] = 0.0096 # Units: mg mm^-3 (Empirical weight - length regression; not
 pars["yRP"] = pars["yRP"]*17.5 # new yRP = 0.824 * 17.5 (1-8-19)( # old yRP = 0.0471 * (1/0.4) * 7 [expected shedding output per weekly shed * (snails shed 40% of their total cercariae during 9-11 AM.) * 7 days] 
 pars["hb"] = 0.001
 pars["Hatch"] = 0.5
+#pars["P"] = 1 # predator density
+# pars["PA"] = 1 # predator attack rate units = ENV day-1 (50L sweeped / day or 8.33 * 6 L tanks (from sokolow etal 2014 acta tropica))
+# pars["Ph"] = 0.1 # predator handling time; units = day
+
 
 ######
 
@@ -85,7 +86,7 @@ Infection = function(snail.stats, miracidia, parameters){
   step = as.numeric(parameters["step"])
   
   # Later calculations depend on exposure probabilities
-  exp.rates =epsilon/ENV*(snail.stats[,"L"]>0) # This is just to get uniform exposure rates
+  exp.rates =epsilon/ENV*(snail.stats[,"L"]>0) # This gives uniform exposure rates for all snails
   sum.exp.rates = sum(exp.rates)
   
   # Probabilities for fate of miracidia
@@ -102,90 +103,110 @@ Infection = function(snail.stats, miracidia, parameters){
 }
 
 
-# Start controlling NetLogo
-state = Initialize_IBM(60, 2, 16, 1)
-n.ticks = 112
-Env_G = numeric()
-snail.IDs = length(state$Snails[[1]][,"ID"])
-
-for(t in 1:n.ticks){
-  # Daily check for time-varying parameters
-  #pars["Det"] = ifelse(t %% pars["pulse_freq"] == 0, 1, 0) 
-  pars["M_in"] = ifelse( t %in% c(7, 21, 42, 63), 150, 0)
+run_DEB_IBM = function(N.snail=60, min.L=2, max.L=16, Food=1, n.ticks=112, drip=T){
   
-  # Get environmental stats
-  environment = c("F" = state$Env_F[t], "M" = state$Env_M[t], "Z" = state$Env_Z[t], "G" = state$Env_G[t])
+  # Set up initial conditions, data structures
+  state = Initialize_IBM(N.snail,min.L, max.L, Food)
+  Env_G = numeric()
+  snail.IDs = length(state$Snails[[1]][,"ID"])
   
-  # Simulate snails
-  if(dim(state$Snails[[t]])[1] > 0){
-    # set host variables
-    snail.stats = state$Snails[[t]]
-    N.snails = length(snail.stats[,"L"])
-
-    # Infect snails
-    Infection.step = as.vector(Infection(snail.stats, environment[2], pars)) # Who gets infected
-    snail.stats[which(Infection.step[1:N.snails] > 0),"P"] = snail.stats[which(Infection.step[1:N.snails] > 0),"P"] + 2.85e-5 # add biomass of one miracidia
-
-    # Update DEBS, HAZ=0 so survival probs are calculated for the current day
-    snail.update = t(mapply(DEB, L=snail.stats[,"L"], e=snail.stats[,"e"], D=snail.stats[,"D"], RH=snail.stats[,"RH"],
-                            P=snail.stats[,"P"], RP=snail.stats[,"RP"], DAM=snail.stats[,"DAM"], Lp=snail.stats[,"LG"], 
-                            MoreArgs = list(step=1, HAZ=0, Food=as.numeric(environment["F"]),
-                                            iM=pars["iM"], k=pars["k"], M=pars["M"], EM=pars["EM"], Fh=pars["Fh"], 
-                                            muD=pars["muD"],
-                                            DR=pars["DR"], yRP=pars["yRP"], ph=pars["ph"], yPE=pars["yPE"], iPM=pars["iPM"], eh=pars["eh"],
-                                            mP=pars["mP"], alpha=pars["alpha"], yEF=pars["yEF"], LM=pars["LM"], kd=pars["kd"], z=pars["z"], 
-                                            kk=pars["kk"], hb=pars["hb"],theta=pars["theta"], mR=pars["mR"], yVE=pars["yVE"], 
-                                            SAtotal= sum(snail.stats[,"L"]^2), 
-                                            ENV=pars["ENV"], r=pars["r"], K=pars["K"], 
-                                            Det=pars["Det"]))) # detritus (Det) defined in C file
+  # run the IBM
+  for(t in 1:n.ticks){
+    #Daily check for time-varying parameters
+    if(drip == F){pars["M_in"] = ifelse( t %in% c(7, 21, 42, 63), 150, 0)}
     
-    snail.update[is.nan(snail.update)] <- 0 # turn nans from matrix into 0
+    # Get environmental stats
+    environment = c("F" = state$Env_F[t], "M" = state$Env_M[t], "Z" = state$Env_Z[t], "G" = state$Env_G[t])
     
-    # Calculate some useful individual-level stuff
-    DEBmass = pars["M"]*((1 + snail.update[,"e"]*pars["EM"])/(1 + pars["EM"]))*snail.update[,"L"]^3
-    Appmass = pars["DV"]*snail.update[,"LG"]^3
-    
-    Eggs = floor(snail.update[,"RH"]/0.015)  # Figure out how many (whole) eggs are released
-    snail.update[,"RH"] = snail.update[,"RH"] %% 0.015        # Remove released cercariae from the buffer
-    Cercs = floor(snail.update[,"RP"]/4e-5)  # Figure out how many (whole) cercs are released
-    snail.update[,"RP"] = snail.update[,"RP"] %% 4e-5         # Remove released cercariae from buffer
-    
-    # Store calculated individual-level stuff
-    snail.update = cbind(snail.update, "DEBmass" = DEBmass, "Appmass" = Appmass, "repro" = Eggs, "Cercs" = Cercs, "t" = rep(t, times=length(Cercs)))
-    
-    # Snails have to survive the day
-    mortality.luck = runif(N.snails, min=0, max=1)
-    surviving = mortality.luck >= 1 - exp(-snail.update[,"HAZ"])
-    state$Snails[[t+1]] = cbind("ID" = snail.stats[which(surviving == TRUE), "ID"],  snail.update[which(surviving == TRUE),c("L", "e", "D", "RH", "P", "RP", "DAM", "HAZ", "LG", "DEBmass", "Appmass", "repro", "Cercs", "t")])
-    
-    # Update environment
-    state$Env_F[t+1] = max(0, snail.update[1,"Food"])
-    state$Env_M[t+1] = as.numeric(Infection.step[N.snails + 1] + pars["M_in"]) # total miracidia density 
-    state$Env_Z[t+1] = as.numeric(environment[3]*exp(-pars["m_Z"]*pars["step"]) + sum(Cercs)/pars["ENV"]) # total cerc density
-
+    # Simulate snails
+    if(dim(state$Snails[[t]])[1] > 0){
+      # set host variables
+      snail.stats = state$Snails[[t]]
+      N.snails = length(snail.stats[,"L"])
+      
+      # Infect snails
+      Infection.step = as.vector(Infection(snail.stats, environment[2], pars)) # Who gets infected
+      snail.stats[which(Infection.step[1:N.snails] > 0),"P"] = snail.stats[which(Infection.step[1:N.snails] > 0),"P"] + 2.85e-5 # add biomass of one miracidia
+      
+      # Update DEBS, HAZ=0 so survival probs are calculated for the current day
+      snail.update = t(mapply(DEB, L=snail.stats[,"L"], e=snail.stats[,"e"], D=snail.stats[,"D"], RH=snail.stats[,"RH"],
+                              P=snail.stats[,"P"], RP=snail.stats[,"RP"], DAM=snail.stats[,"DAM"], Lp=snail.stats[,"LG"], 
+                              MoreArgs = list(step=1, HAZ=0, Food=as.numeric(environment["F"]),
+                                              iM=pars["iM"], k=pars["k"], M=pars["M"], EM=pars["EM"], Fh=pars["Fh"], 
+                                              muD=pars["muD"],
+                                              DR=pars["DR"], yRP=pars["yRP"], ph=pars["ph"], yPE=pars["yPE"], iPM=pars["iPM"], eh=pars["eh"],
+                                              mP=pars["mP"], alpha=pars["alpha"], yEF=pars["yEF"], LM=pars["LM"], kd=pars["kd"], z=pars["z"], 
+                                              kk=pars["kk"], hb=pars["hb"],theta=pars["theta"], mR=pars["mR"], yVE=pars["yVE"], 
+                                              SAtotal= sum(snail.stats[,"L"]^2), 
+                                              ENV=pars["ENV"], r=pars["r"], K=pars["K"], 
+                                              Det=pars["Det"]))) # detritus (Det) defined in C file
+      
+      snail.update[is.nan(snail.update)] <- 0 # turn nans from matrix into 0
+      
+      # Calculate some useful individual-level stuff
+      DEBmass = pars["M"]*((1 + snail.update[,"e"]*pars["EM"])/(1 + pars["EM"]))*snail.update[,"L"]^3
+      Appmass = pars["DV"]*snail.update[,"LG"]^3
+      
+      Eggs = floor(snail.update[,"RH"]/0.015)  # Figure out how many (whole) eggs are released
+      snail.update[,"RH"] = snail.update[,"RH"] %% 0.015        # Remove released cercariae from the buffer
+      Cercs = floor(snail.update[,"RP"]/4e-5)  # Figure out how many (whole) cercs are released
+      snail.update[,"RP"] = snail.update[,"RP"] %% 4e-5         # Remove released cercariae from buffer
+      
+      # Store calculated individual-level stuff
+      snail.update = cbind(snail.update, "DEBmass" = DEBmass, "Appmass" = Appmass, "repro" = Eggs, "Cercs" = Cercs, "t" = rep(t, times=length(Cercs)))
+      
+      # Snails have to survive the day
+      mortality.luck = runif(N.snails, min=0, max=1)
+      
+      # hb <- hb + (pred_a * pred_p) / (1 + pred_a * pred_h * N.snails)
+      surviving = mortality.luck >= 1 - exp(-snail.update[,"HAZ"])
+      state$Snails[[t+1]] = cbind("ID" = snail.stats[which(surviving == TRUE), "ID"],  snail.update[which(surviving == TRUE),c("L", "e", "D", "RH", "P", "RP", "DAM", "HAZ", "LG", "DEBmass", "Appmass", "repro", "Cercs", "t")])
+      
+      # Update environment
+      state$Env_F[t+1] = max(0, snail.update[1,"Food"])
+      state$Env_M[t+1] = as.numeric(Infection.step[N.snails + 1] + pars["M_in"]) # total miracidia density 
+      state$Env_Z[t+1] = as.numeric(environment[3]*exp(-pars["m_Z"]*pars["step"]) + sum(Cercs)/pars["ENV"]) # total cerc density
+      
     }else{ # if there are no hosts  
-    
-    state$Env_M[t+1] = as.numeric(environment[2]*exp(-pars["m_M"]*pars["step"]) + pars["M_in"]) # total miracidia density 
-    state$Env_Z[t+1] = as.numeric(environment[3]*exp(-pars["m_Z"]*pars["step"])) # total cerc density
-    state$Env_F[t+1] = ifelse(pars["Det"] == 0, as.numeric(pars["K"]*environment[1]/(environment[1] + (pars["K"] - environment[1])*exp(-pars["r"]*pars["step"]))), as.numeric(environment[1] + pars["Det"]))
-    Eggs <- 0
+      
+      state$Env_M[t+1] = as.numeric(environment[2]*exp(-pars["m_M"]*pars["step"]) + pars["M_in"]) # total miracidia density 
+      state$Env_Z[t+1] = as.numeric(environment[3]*exp(-pars["m_Z"]*pars["step"])) # total cerc density
+      state$Env_F[t+1] = ifelse(pars["Det"] == 0, as.numeric(pars["K"]*environment[1]/(environment[1] + (pars["K"] - environment[1])*exp(-pars["r"]*pars["step"]))), as.numeric(environment[1] + pars["Det"]))
+      Eggs <- 0
     }
     
-  state$Env_G[t+1] = max(0, sum(Eggs))
-
-  if(t > 10){
-    births =  rbinom(n=1, size=state$Env_G[t - 10], prob=pars["Hatch"])
-    if(births > 0){
-      neonates = cbind("ID" = (snail.IDs+1):(snail.IDs + births), "L" = rep(0.75, births), "e" = rep(0.9, times=births), "D" = rep(0, times=births), "RH" = rep(0, times=births),
-                  "P" = rep(0, times=births), "RP" = rep(0, times=births), "DAM" = rep(0, times=births),
-                  "HAZ" = rep(0, times=births), "LG" = rep(0.75, births), "DEBmass" = pars["M"]*((1 + 0.9*pars["EM"])/(1 + pars["EM"]))*0.75^3,
-                  "Appmass" = pars["DV"]*0.75^3, "repro" = rep(0, times=births), "Cercs" = rep(0, times=births), "t" = rep(t, times=births))
-      snail.IDs = snail.IDs + births # Updates the running count of snails
-      state$Snails[[t+1]] = rbind(state$Snails[[t+1]], neonates)
+    state$Env_G[t+1] = max(0, sum(Eggs))
+    
+    if(t > 10){
+      births =  rbinom(n=1, size=state$Env_G[t - 10], prob=pars["Hatch"])
+      if(births > 0){
+        neonates = cbind("ID" = (snail.IDs+1):(snail.IDs + births), "L" = rep(0.75, births), "e" = rep(0.9, times=births), "D" = rep(0, times=births), "RH" = rep(0, times=births),
+                         "P" = rep(0, times=births), "RP" = rep(0, times=births), "DAM" = rep(0, times=births),
+                         "HAZ" = rep(0, times=births), "LG" = rep(0.75, births), "DEBmass" = pars["M"]*((1 + 0.9*pars["EM"])/(1 + pars["EM"]))*0.75^3,
+                         "Appmass" = pars["DV"]*0.75^3, "repro" = rep(0, times=births), "Cercs" = rep(0, times=births), "t" = rep(t, times=births))
+        snail.IDs = snail.IDs + births # Updates the running count of snails
+        state$Snails[[t+1]] = rbind(state$Snails[[t+1]], neonates)
+      }
     }
+    print(t)
   }
-  print(t)
+  return(state)
 }
+
+
+state = run_DEB_IBM()
+
+### Some functions to process the output ###
+catch_snails = function(mat, LG = 2){
+  mat[which(mat[,"LG"] >= LG),]
+}
+
+catch_state = function(state){
+  caught_snails = lapply(state$Snails, FUN=catch_snails)
+  list("Snails" = caught_snails, "Env_F" = state$Env_F, "Env_M" = state$Env_M, "Env_Z" = state$Env_Z, "Env_G" = state$Env_G)
+}
+
+caught_state = catch_state(state)
 
 digest_state = function(state){
   span = length(state$Env_F)
@@ -197,9 +218,12 @@ digest_state = function(state){
 }
 
 
-
+# Full pairwise plots of pop-level outcomes
+plot(digest_state(caught_state))
 plot(digest_state(state))
 
+
+# Finding all infected individuals
 digest_i_states = function(state){
   all_inds = do.call(rbind, state$Snails)
   inf_inds = subset(data.frame(all_inds), Cercs > 0)
@@ -209,11 +233,43 @@ digest_i_states = function(state){
 pop.stats = digest_state(state)
 inf.snails = digest_i_states(state)
 
-library(ggplot2)
-p.cercs = ggplot(data=inf.snails, aes(x=t, y=Cercs, group=ID, color=ID)) + geom_line()
-p.Snails = ggplot(data=pop.stats, aes(x=time, y=Snails)) + geom_line()
+# A function suitable for replicate() to handle and return the pop-level summarized output
+summarized_DEB_IBM = function(N.snail=60, min.L=2, max.L=16, Food=1, n.ticks=112, drip=T){
+  digest_state(run_DEB_IBM(N.snail, min.L, max.L, Food, n.ticks, drip))
+}
 
-snail_avgs = matrix(unlist(lapply(state$Snails, FUN=colMeans)), nrow=length(state$Snails), ncol=dim(state$Snails[[1]])[2], byrow=T)
-colnames(snail_avgs) = c("ID", "L", "e", "D", "RH", "P", "RP", "DAM", "HAZ", "LG", "DEBmass", "Appmass", "repro", "Cercs", "t")
-snail_avgs
-plot(0:n.ticks, snail_avgs[,"e"], typ="l")
+# Runs replicate simulations with the same conditions
+rep_states_drip = replicate(n=50, summarized_DEB_IBM(), simplify=T)
+rep_states_pulse = replicate(n=50, summarized_DEB_IBM(drip=F), simplify=T)
+
+p = ggplot()
+for(i in 1: dim(rep_states_drip)[2]){p = p + geom_line(data=data.frame(rep_states_drip[,i]), aes(x=time, y=Cercariae))}
+p
+
+for(i in 1: dim(rep_states_pulse)[2]){p = p + geom_line(data=data.frame(rep_states_pulse[,i]), aes(x=time, y=Cercariae), colour="red")}
+p
+
+p2 = ggplot()
+for(i in 1: dim(rep_states_drip)[2]){p2 = p2 + geom_line(data=data.frame(rep_states_drip[,i]), aes(x=time, y=Snails))}
+for(i in 1: dim(rep_states_pulse)[2]){p2 = p2 + geom_line(data=data.frame(rep_states_pulse[,i]), aes(x=time, y=Snails), colour="red")}
+p2
+
+p3 = ggplot()
+for(i in 1: dim(rep_states_drip)[2]){p3 = p3 + geom_line(data=data.frame(rep_states_drip[,i]), aes(x=time, y=Food))}
+for(i in 1: dim(rep_states_pulse)[2]){p3 = p3 + geom_line(data=data.frame(rep_states_pulse[,i]), aes(x=time, y=Food), colour="red")}
+p3
+
+p4 = ggplot()
+for(i in 1: dim(rep_states_drip)[2]){p4 = p4 + geom_line(data=data.frame(rep_states_drip[,i]), aes(x=time, y=Miracidia))}
+for(i in 1: dim(rep_states_pulse)[2]){p4 = p4 + geom_line(data=data.frame(rep_states_pulse[,i]), aes(x=time, y=Miracidia), colour="red")}
+p4
+
+
+# library(ggplot2)
+# p.cercs = ggplot(data=inf.snails, aes(x=t, y=Cercs, group=ID, color=ID)) + geom_line()
+# p.Snails = ggplot(data=pop.stats, aes(x=time, y=Snails)) + geom_line()
+# 
+# snail_avgs = matrix(unlist(lapply(state$Snails, FUN=colMeans)), nrow=length(state$Snails), ncol=dim(state$Snails[[1]])[2], byrow=T)
+# colnames(snail_avgs) = c("ID", "L", "e", "D", "RH", "P", "RP", "DAM", "HAZ", "LG", "DEBmass", "Appmass", "repro", "Cercs", "t")
+# snail_avgs
+# plot(0:n.ticks, snail_avgs[,"e"], typ="l")
